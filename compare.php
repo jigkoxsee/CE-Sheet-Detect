460ee642964fc1b9c91f3da3f116d8d53feec2b9
jigkoxsee <ked.naja@gmail.com>
2013-11-18 13:05:54 +0700

    add connect db

--- compare.php
+++ compare.php
@@ -1,8 +1,13 @@
 <?php
+	// funtion to check is JSON is complete
     function isJson($string) {
         json_decode($string);
         return (json_last_error() == JSON_ERROR_NONE);
     }
-    echo "hello JSON<br/>";
-    echo isJson($_POST['data']);
+    $data=$_POST['data'];
+    echo "hello JSON";
+    echo isJson($data);
+    $dat=json_encode($data);
+    // compare $data with in MySQL db.
+    include 'connectdb.php'
 ?>
