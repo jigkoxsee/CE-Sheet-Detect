5c7e6a8a3b9de2f14c0da18a9ee71bb623368c80
jigkoxsee <ked.naja@gmail.com>
2013-11-18 20:05:14 +0700

    fetch data in MySQL

--- compare.php
+++ compare.php
@@ -6,8 +6,30 @@
     }
     $data=$_POST['data'];
     echo "hello JSON";
-    echo isJson($data);
-    $dat=json_encode($data);
+    echo isJson($data)."\n";
+    $data=json_encode($data,true);
+
+
     // compare $data with in MySQL db.
-    include 'connectdb.php'
+$con=mysqli_connect("localhost","root","forgetpassword","ziko");
+// Check connection
+if (mysqli_connect_errno())
+  {
+  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+  }
+else
+  echo "Connect DB success";
+//comparator
+$subject_id=6; //TODO(ziko):get value from post method
+$year=2013; //TODO(ziko):same above
+// MySQL for testing
+$result = mysqli_query($con,"SELECT * FROM tbl_sheet WHERE subject_id ='".$subject_id."' AND year ='".$year."'");
+echo "<br/>";
+while($row = mysqli_fetch_array($result))
+  {
+  echo $row['orientation'] . " " . $row['link'];
+  echo "<br/>";
+  }
+
+mysqli_close($con);
 ?>
--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 /**
-* Created by ziko on 11/13/13.
-*/
+ * Created by ziko on 11/13/13.
+ */
 
 /*subject page*/
 var subject=new Array();
@@ -10,59 +10,63 @@ subject[2]="http://www.ce.kmitl.ac.th/subject.php?action=view&SUBJECT_ID=150";//
 subject[3]="http://www.ce.kmitl.ac.th/subject.php?action=view&SUBJECT_ID=154";// prob & stat
 
 var infn=new Object();
-$.get(subject[0],
-    function(data) {
-        var subject_id=6;
-        var allsheet=new Array();
+function sheet_detection(webpage_file){
+    $.get(webpage_file,
+        function(data) {
+            var subject_id=6;
+            var allsheet=new Array();
 
-        document.write(data);
+            document.write(data);
 
-        var sheet_table =document.getElementsByTagName('table')[15];
-        $('body').empty();
-        var sheet_row   =sheet_table.getElementsByTagName('tr');
-        alert(sheet_row.length);
+            var sheet_table =document.getElementsByTagName('table')[15];
+            $('body').empty();
+            var sheet_row   =sheet_table.getElementsByTagName('tr');
+            alert(sheet_row.length);
 
-        infn=sheet_row[2];
+            infn=sheet_row[2];
 
-        $('body').append("<p>"+sheet_row[0].innerHTML+"</p>");// table HEAD is here
-        var sheet_in_row   =sheet_row[0].getElementsByTagName('td');
-        var row_length=sheet_in_row.length; // cal table row length
-        //alert(row_length);
-        //$("body").append("<p>"+sheet_in_row[i].innerHTML+"</p>");
-        for(var i=1;i<sheet_row.length;i++){
-            //$('body').append("<p>"+sheet_row[i].innerHTML+"</p>"); //print for test
-            //console.log(sheet_row[i].innerHTML);
+            $('body').append("<p>"+sheet_row[0].innerHTML+"</p>");// table HEAD is here
+            var sheet_in_row   =sheet_row[0].getElementsByTagName('td');
+            var row_length=sheet_in_row.length; // cal table row length
+            //alert(row_length);
+            //$("body").append("<p>"+sheet_in_row[i].innerHTML+"</p>");
+            for(var i=1;i<sheet_row.length;i++){
+                //$('body').append("<p>"+sheet_row[i].innerHTML+"</p>"); //print for test
+                //console.log(sheet_row[i].innerHTML);
 
-            $('body').append("<p>"+sheet_row[i].getElementsByTagName('td')[1].innerHTML+"</p>");//get orientation here
-            for(var j=2;j<row_length;j++){
-                item=sheet_row[i].getElementsByTagName('td')[j];
-                item_link=item.getElementsByTagName('a');
-                //console.log(i+":"+item_link.length);
-                if(item_link.length){
-                    $("body").append("<a href=\"http://www.ce.kmitl.ac.th"+item_link.item(0).getAttribute('href')+"\">link</a><br/>");
-                    var sheet   =new Object();
-                    sheet.subject_id=subject_id;
-                    sheet.orientation=sheet_row[i].getElementsByTagName('td')[1].innerHTML;//add filename
-                    sheet.f_num=parseInt(parseInt(j)-2);
-                    //console.log(sheet.f_num);
-                    sheet.link="<a href=\"http://www.ce.kmitl.ac.th"+item_link.item(0).getAttribute('href')+"\">link</a>";
-                    allsheet.push(sheet);
-                    console.log("al : "+allsheet.length+" / "+sheet.orientation);
+                $('body').append("<p>"+sheet_row[i].getElementsByTagName('td')[1].innerHTML+"</p>");//get orientation here
+                for(var j=2;j<row_length;j++){
+                    item=sheet_row[i].getElementsByTagName('td')[j];
+                    item_link=item.getElementsByTagName('a');
+                    //console.log(i+":"+item_link.length);
+                    if(item_link.length){
+                        $("body").append('<a href="http://www.ce.kmitl.ac.th"'+item_link.item(0).getAttribute('href')+'">link</a><br/>');
+                        var sheet   =new Object();
+                        sheet.subject_id=subject_id;
+                        sheet.orientation=sheet_row[i].getElementsByTagName('td')[1].innerHTML;//add filename
+                        sheet.f_num=parseInt(parseInt(j)-2);
+                        //console.log(sheet.f_num);
+                        sheet.link='<a href="http://www.ce.kmitl.ac.th/"'+item_link.item(0).getAttribute('href')+'>link</a>';
+                        allsheet.push(sheet);
+                        console.log("al : "+allsheet.length+" / "+sheet.orientation);
+                    }
+                    //$('body').append("<p>"+parseInt(parseInt(j)-parseInt(2))+":"+item.innerHTML+"</p>");//for test each element
                 }
-                //$('body').append("<p>"+parseInt(parseInt(j)-parseInt(2))+":"+item.innerHTML+"</p>");//for test each element
+                $("body").append("<hr/>");
+
             }
-            $("body").append("<hr/>");
+            console.log(JSON.stringify(allsheet));
+            $.post("compare.php",
+                {
+                    data:JSON.stringify(allsheet)
+                },
+                function(data,status){
+                    //alert("Data: " + data + "\nStatus: " + status);
+                    $('body').empty();
+                    $('body').append(data);
+                });
 
         }
-        console.log(JSON.stringify(allsheet));
-        $.post("compare.php",
-            {
-                data:JSON.stringify(allsheet)
-            },
-            function(data,status){
-                alert("Data: " + data + "\nStatus: " + status);
-            });
-
-    }
-);
-
+    );
+}
+sheet_detection(subject[0]);
